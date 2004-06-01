f986f6fa65da33021989c72135a38d1cc5ee33c2
Aimée Vargas Estrada <vaimee@yahoo.com>
2004-06-01 18:49:31 +0000

    Altered Configuration function to support articulated objects
    git-svn-id: svn+ssh://parasol-svn.cs.tamu.edu/research/parasol-svn/svnrepository/vizmo/trunk@98 8e255c04-9930-0410-9bf1-9572f6c5958d

--- vizmo2/src/EnvObj/Robot.h
+++ vizmo2/src/EnvObj/Robot.h
@@ -6,7 +6,13 @@
 #include <EnvObj/MultiBodyModel.h>
 #include <EnvObj/EnvLoader.h>
 #include <GLModel.h>
+#include <EnvObj/MultiBodyInfo.h>
+#include <EnvObj/Transformation.h>
 using namespace plum;
+
+#include<Vector.h>
+#include<Matrix.h>
+using namespace mathtool;
 
 class OBPRMView_Robot : public CGLModel
 {
@@ -38,33 +44,48 @@ public:
         if(m_RobotModel!=NULL) m_RobotModel->SetColor(r,g,b,a);
     }
 
-	virtual const string GetName() const { 
-		if( m_RobotModel!=NULL ) return "Robot";
-		return "No Robot"; 
-	}
-
-	virtual void GetChildren( list<CGLModel*>& models ){ 
-		if( m_RobotModel!=NULL )
-			models.push_back(m_RobotModel);
-	}
+    virtual const string GetName() const { 
+      if( m_RobotModel!=NULL ) return "Robot";
+      return "No Robot"; 
+    }
+    
+    virtual void GetChildren( list<CGLModel*>& models ){ 
+      if( m_RobotModel!=NULL )
+	models.push_back(m_RobotModel);
+    }
+    
 
     //////////////////////////////////////////////////////////////////////
     // Access Functions
     //////////////////////////////////////////////////////////////////////
-    void Configure( double * cfg ) { //assum this is rigid
-        //setup rotation and translation
-        tx()=cfg[0]; ty()=cfg[1]; tz()=cfg[2];
-        rx()=cfg[3]; ry()=cfg[4]; rz()=cfg[5];
-        Euiler2Quaternion();
-    }
+    void Configure( double * cfg );
+    
+/*     void Configure( double * cfg ) { //assum this is rigid */
+/*         //setup rotation and translation */
+/*         tx()=cfg[0]; ty()=cfg[1]; tz()=cfg[2]; */
+/*         rx()=cfg[3]; ry()=cfg[4]; rz()=cfg[5]; */
+/*         Euiler2Quaternion(); */
+/*     } */
         
     CEnvLoader * getEnvLoader() const;
     CMultiBodyModel * getRobotModel() const;
-	void Scale(double x, double y, double z)
-	{
-		if(m_RobotModel!=NULL) 
-			m_RobotModel->Scale(x,y,z);
-	}
+
+    void BackUp();
+    void Restore();
+
+    //variable used to change the size of the robot
+    float size; 
+    bool change;
+    char come;
+    double R, G, B, o_s[3], x, y, z, x1, y1, z1;
+
+    Quaternion quat, q1, q2;
+
+    void Scale(double x, double y, double z)
+      {
+	if(m_RobotModel!=NULL) 
+	  m_RobotModel->Scale(x,y,z);
+      }
     
     //////////////////////////////////////////////////////////////////////
     // Private Stuff
@@ -73,6 +94,10 @@ private:
     
     CEnvLoader * m_pEnvLoader;
     CMultiBodyModel * m_RobotModel;
+    CMultiBodyInfo * m_RobotInfo;
+
+    CPolyhedronModel * m_Poly, * pPolyBackUp, * Poly;
+
 };
 
 #endif
