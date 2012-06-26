3fa934884dfb6ae5b6a719e40c10c61ec7a5365e
Nicole Violet Julian <nicolevioletj@tamu.edu>
2012-06-26 17:18:26 +0000

    Fixing compile plus some standards.
    git-svn-id: svn+ssh://parasol-svn.cs.tamu.edu/research/parasol-svn/svnrepository/vizmo/trunk@254 8e255c04-9930-0410-9bf1-9572f6c5958d

--- src/gui/GUI.cpp
+++ src/gui/GUI.cpp
@@ -1,31 +1,30 @@
 #include "GUI.h"
-#include "main_win.h"
+#include "MainWin.h"
 #include <glut.h>
 #include <iostream>
 ///////////////////////////////////////////////////////////////////////////////
 // Qt Headers
 #include <qapplication.h>
 
-VizmoMainWin * CreatWindow(int argc, char ** argv)
-{
-   VizmoMainWin * win=new VizmoMainWin();
+VizmoMainWin* CreatWindow(int _argc, char** _argv) {
+   VizmoMainWin* win=new VizmoMainWin();
    //check file name
-   vector<string> args; args.reserve(argc-1);
-   for( int i=1;i<argc;i++ )
-      args.push_back(argv[i]);
+   vector<string> args; args.reserve(_argc-1);
+   for( int i=1;i<_argc;i++ )
+      args.push_back(_argv[i]);
    if( win->Init()==false ){ delete win; return NULL; }
    win->SetArgs(args);
    return win;
 }
 
 ///Initialize gui objects, such as creating windows, toolbars, ...etc.
-bool VizGUI_Init(int argc, char ** argv)
+bool VizGUI_Init(int _argc, char** _argv)
 {
-   glutInit(&argc,argv);
+   glutInit(&_argc,_argv);
    QApplication::setColorSpec( QApplication::CustomColor );
-   QApplication app(argc, argv);
+   QApplication app(_argc, _argv);
 
-   VizmoMainWin * win=CreatWindow(argc, argv);
+   VizmoMainWin* win=CreatWindow(_argc, _argv);
    if( win==NULL ) return false;
    win->show();
    app.exec();
