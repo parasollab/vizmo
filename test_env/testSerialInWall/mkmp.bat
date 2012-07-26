5628a4b0ec99fd2cfccd631b7a06fc9e33524a66
Jory Denny <jdenny@cse.tamu.edu>
2012-07-26 21:29:00 +0000

    Removing old test folder.
    git-svn-id: svn+ssh://parasol-svn.cs.tamu.edu/research/parasol-svn/svnrepository/vizmo/trunk@266 8e255c04-9930-0410-9bf1-9572f6c5958d

--- test_env/testSerialInWall/mkmp.bat
+++
@@ -1,2 +0,0 @@
-..\obprm -cd RAPID -f serial -cfg Cfg_free_tree 3   -bbox [0.0,4.0,0.0,4.0,-5.0,14.0]  -bbox_scale 1  -lp straightline rotate_at_s  -nodes 200  -gNodes OBPRM  -collPair N_rT N_rT -nshells 1 -freePair rT rT  -cNodes closest 20 components 5 5 
-
