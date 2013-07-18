e582dbbbb161b7340833d1bad076f1aaba836c42
Jory Denny <jdenny@cse.tamu.edu>
2013-07-18 19:15:56 +0000

    Moving Phantom code into other folder. This code is not compiled at this time.
    git-svn-id: svn+ssh://parasol-svn.cs.tamu.edu/research/parasol-svn/svnrepository/vizmo/trunk@378 8e255c04-9930-0410-9bf1-9572f6c5958d

--- src/vizmo2.h
+++ src/vizmo2.h
@@ -1,5 +1,5 @@
-#if !defined(_VIZMO2_H_)
-#define _VIZMO2_H_
+#ifndef VIZMO_H_
+#define VIZMO_H_
 
 #include <vector>
 #include <string>
@@ -11,18 +11,6 @@
 #include <Plum/EnvObj/MultiBodyModel.h>
 #include "Models/MapModel.h"
 #include <Utilities/CollisionDetection.h>
-
-#ifdef USE_PHANTOM
-#include <HD/hd.h>
-#include <HDU/hduError.h>
-#include <HDU/hduVector.h>
-//#include <HL/hl.h>
-//#include <HDU/hduMath.h>
-//#include <HDU/hduMatrix.h>
-//#include <HDU/hduQuaternion.h>
-//#include <HDU/hduError.h>
-//#include <HLU/hlu.h>
-#endif
 
 namespace plum{
   class CPlum;
@@ -37,113 +25,6 @@ void SetCamera(gliCamera* s_camera);
 
 class OBPRMView_Robot;
 OBPRMView_Robot* GetRobot();
-
-#ifdef USE_PHANTOM
-//Define singleton
-class PhantomManager;
-PhantomManager& GetPhantomManager();
-
-//static HHLRC ghHLRC = NULL;
-static HHD ghHD = HD_INVALID_HANDLE;
-static HDSchedulerHandle gSchedulerCallback = HD_INVALID_HANDLE;
-
-/* Haptic device record. */
-struct DeviceDisplayState
-{
-   HHD m_hHD;
-   hduVector3Dd position;
-   hduVector3Dd velocity;
-   hduVector3Dd force;
-   hduVector3Dd rotation;
-};
-
-void exitHandler();
-
-/*******************************************************************************
-  Client callback.
-  Use this callback synchronously.
-  Gets data, in a thread safe manner, that is constantly being modified by the
-  haptics thread.
- *******************************************************************************/
-HDCallbackCode HDCALLBACK DeviceStateCallback(void *pUserData);
-
-/*******************************************************************************
-  Main callback that calculates and sets the force.
- *******************************************************************************/
-HDCallbackCode HDCALLBACK MainCallback(void *data);
-
-class PhantomManager
-{
-   /*******************************************************************************
-     Schedules the force callback.
-    *******************************************************************************/
-   void ScheduleForceCallback();
-
-   public:
-   /*******************************************************************************
-     Previous Valid Configuration Data
-    *******************************************************************************/
-   vector<double> validpos;
-
-   /*******************************************************************************
-     Potential Valid Configuration Data
-    *******************************************************************************/
-   vector<double> fpos;
-
-   /*******************************************************************************
-     Collision
-    *******************************************************************************/
-   int Collision;
-
-   /*******************************************************************************
-     Use Feedback?
-    *******************************************************************************/
-   bool UseFeedback;
-
-   /*******************************************************************************
-     CDOn
-    *******************************************************************************/
-   bool CDOn;
-
-   /*******************************************************************************
-     proceed
-    *******************************************************************************/
-   bool proceed;
-
-   /*******************************************************************************
-     Phantom Force Factor
-    *******************************************************************************/
-   double phantomforce;
-
-   /*******************************************************************************
-     Constructor
-    *******************************************************************************/
-   PhantomManager();
-
-   /*******************************************************************************
-     Initialize Phantom Device
-    *******************************************************************************/
-   void initPhantom();
-
-   /*******************************************************************************
-     Get Position of End-Effector
-    *******************************************************************************/
-   hduVector3Dd getEndEffectorPosition();
-
-   /*******************************************************************************
-     Get Roatation
-    *******************************************************************************/
-   hduVector3Dd getRotation();
-
-   /*******************************************************************************
-     Get Velocity of End-Effector
-    *******************************************************************************/
-   hduVector3Dd getVelocity();
-
-};
-
-/////////////////////////////////////////////////////////////////////////////////
-#endif
 
 //Define singleton
 class vizmo;
@@ -423,9 +304,4 @@ class vizmo
       Plum m_Plum;
 };
 
-#endif // !defined(_VIZMO2_H_)
-
-
-
-
-
+#endif
