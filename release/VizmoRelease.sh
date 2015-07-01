c31dfdfbe443faf9cb9f1043a13f58eb4205eeaf
Jory Denny <jdenny@cse.tamu.edu>
2015-07-01 19:49:18 +0000

    Fixing release script.
    git-svn-id: svn+ssh://parasol-svn.cs.tamu.edu/research/parasol-svn/svnrepository/vizmo/trunk@851 8e255c04-9930-0410-9bf1-9572f6c5958d

--- release/VizmoRelease.sh
+++ release/VizmoRelease.sh
@@ -24,9 +24,9 @@ cd $BASENAME
 rm -rf `find -name .svn`
 
 echo Copying spec file
-sed -i "s/define date 0000.00.00/define date $DATE/" Release/vizmo++.spec
-sed -i "s/Version: 0/Version: $VERSION/" Release/vizmo++.spec
-cp Release/vizmo++.spec $RPMBUILDDIR/SPECS
+sed -i "s/define date 0000.00.00/define date $DATE/" release/vizmo++.spec
+sed -i "s/Version: 0/Version: $VERSION/" release/vizmo++.spec
+cp release/vizmo++.spec $RPMBUILDDIR/SPECS
 
 echo Creating TAR file:: $TAR
 cd ..
