89c4c99e566220475118f57742e9f878fed50694
Diane Marie Bernard Uwacu <duwacu@tamu.edu>
2016-07-25 23:28:06 +0000

    merging vizmo src with current version
    git-svn-id: svn+ssh://parasol-svn.cs.tamu.edu/research/parasol-svn/svnrepository/vizmo/branches/LigandBindingSites@1019 8e255c04-9930-0410-9bf1-9572f6c5958d

--- src/Models/SurfaceMultiBodyModel.cpp
+++
@@ -1,8 +0,0 @@
-#include "SurfaceMultiBodyModel.h"
-
-#include "Environment/SurfaceMultiBody.h"
-
-SurfaceMultiBodyModel::
-SurfaceMultiBodyModel(shared_ptr<SurfaceMultiBody> _s) :
-  StaticMultiBodyModel("SurfaceMultiBody", _s), m_surfaceMultiBody(_s) {
-  }
